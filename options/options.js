b8b4ad74f4ae441738c7b71d125725dc6e88561c
Crowdbotics <engineering@crowdbotics.com>
2023-05-18 21:21:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "walking-pals-dev-84415",
+  url: "https://walking-pals-dev-84415.botics.co",
+  api: "https://walking-pals-dev-84415.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
